14429f2f15bccba2d9fcc22dcdbc9a14fdae621e
Tiago Santos <tiagoalmeidasantos04@gmail.comDD>
2024-05-15 18:44:12 -0300

    adding initial crud

--- src/App.js
+++ src/App.js
@@ -12,20 +12,34 @@ import {
 } from './style.js'
 
 function App() {
-
-
+  const [users, setUsers] = useState([]);
+  const [name, setName] = useState([]);
+  const [age, setAge] = useState([]);
+  function AddNewUser() {
+    setUsers([...users, {
+      name,
+      age,
+      id: uuid()
+    }
+    ],)
+  }
+  function catchName(e) {
+    setName(e.target.value)
+  }
+  function catchAge(e) {
+    setAge(e.target.value)
+  }
   return (
     <Container className="App">
-      <Input placeholder='nome' />
-      <Input placeholder='Idade' />
-      <Button>Cadastrar</Button>
-
-      <ul>
-        <li>qaaq</li>
-        <li>qaaq</li>
-        <li>qaaq</li>
-        <li>qaaq</li>
-      </ul>
+      <Input placeholder='nome' onChange={catchName} />
+      <Input placeholder='Idade' onChange={catchAge} />
+      <Button onClick={AddNewUser}>Cadastrar</Button>
+      {users.map(user => (
+        <ul key={user.id}>
+          <li>{user.name}</li>
+          <li>{user.age}</li>
+        </ul>
+      ))}
     </Container >
   );
 }
