d556302bf9cc662e083c37ee4a0d1e0a903e2793
Tiago Santos <tiagoalmeidasantos04@gmail.com>
2024-07-02 14:03:48 -0300

    commit deleting and showing users

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { useState, useRef, useEffect } from 'react';
-import { v4 as uuid } from 'uuid';
-import axios from 'axios';
+import React, { useState, useRef, useEffect } from "react";
+import { v4 as uuid } from "uuid";
+import axios from "axios";
 import {
   Container,
   Form,
@@ -9,8 +9,8 @@ import {
   ToDoList,
   ListItem,
   Trash,
-  EditUser
-} from './style';
+  EditUser,
+} from "./style";
 
 function App() {
   const [users, setUsers] = useState([]);
@@ -18,33 +18,32 @@ function App() {
   const ageRef = useRef();
 
   async function addNewUser() {
-    const data = await axios.post('http://localhost:3001/user', {
-      name: nameRef.current.value, age: ageRef.current.value
+    const { data: newUsers } = await axios.post("http://localhost:3001/user", {
+      name: nameRef.current.value,
+      age: ageRef.current.value,
     });
-    console.log(data);
-    setUsers([...users, { name: data.data.name, age: data.data.age, id: data.data.id }]);
+    setUsers([...users, newUsers]);
     nameRef.current.value = "";
     ageRef.current.value = "";
-
-  }
-  async function ShowUsersfromData() {
-    const { data } = await axios.get('http://localhost:3001/user'
-    );
-
-    setUsers([{
-      name: data.name, age: data.age, id: data.id
-    }])
   }
 
+  useEffect(() => {
+    async function fetchUsers() {
+      const { data: newUsers } = await axios.get(" http://localhost:3001/user");
+      setUsers(newUsers);
+      console.log(newUsers);
+    }
+    fetchUsers();
+  }, []);
 
   function findUser(id) {
-    const index = users.findIndex(user => user.id === id);
+    const index = users.findIndex((user) => user.id === id);
     return index !== -1 ? { user: users[index], index } : null;
   }
 
-
-
-  function deleteUser(idUser) {
+  async function deleteUser(idUser) {
+    const data = await axios.delete(" http://localhost:3001/user/" + idUser);
+    console.log(data);
     const userIndex = findUser(idUser)?.index;
     if (userIndex !== undefined) {
       const updatedUsers = [...users];
@@ -60,7 +59,11 @@ function App() {
       const updatedAge = window.prompt("Mude a idade", userDetail.user.age);
       if (updatedName !== null && updatedAge !== null) {
         const updatedUsers = [...users];
-        updatedUsers[userDetail.index] = { ...userDetail.user, name: updatedName, age: updatedAge };
+        updatedUsers[userDetail.index] = {
+          ...userDetail.user,
+          name: updatedName,
+          age: updatedAge,
+        };
         setUsers(updatedUsers);
       }
     } else {
@@ -71,12 +74,12 @@ function App() {
   return (
     <Container>
       <Form>
-        <Input type="text" placeholder='Nome' ref={nameRef} />
-        <Input type="text" placeholder='Idade' ref={ageRef} />
+        <Input type="text" placeholder="Nome" ref={nameRef} />
+        <Input type="text" placeholder="Idade" ref={ageRef} />
         <Button onClick={addNewUser}>Cadastrar</Button>
       </Form>
       <ToDoList>
-        {users.map(user => (
+        {users.map((user) => (
           <ListItem key={user.id}>
             <span>{user.name}</span>
             <span>{user.age}</span>
